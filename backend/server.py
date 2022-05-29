aa5cdbd154774299f8fe76cd1a66a36d99e7c051
Klay Clarke <clarkeklay@gmail.com>
2022-05-29 17:08:02 -0400

    add class for Videos, apply cors middleware to ensure access, add get route that transfers video info via json

--- backend/server.py
+++ backend/server.py
@@ -1,19 +1,61 @@
+import os
 import boto3
 import psycopg2
 from typing import List
+from dotenv import load_dotenv
 from pydantic import BaseModel
 import uvicorn
 from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(debug=True)
 
-# create endpoint
+load_dotenv()
+
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+
+class VideoModel(BaseModel):
+    id: int
+    video_title: str
+    video_url: str
+
+
+app = FastAPI(debug=True)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/status")
 async def check_status():
-    return "Hello World"
+    return ("Hello, " + POSTGRES_USER)
 
+
+@app.get("/videos", response_model=List[VideoModel])
+async def get_videos():
+    # connect to database
+    conn = psycopg2.connect(
+        database=POSTGRES_DB, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host="localhost",
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM video ORDER BY id DESC")
+    rows = cur.fetchall()
+
+    formatted_videos = []
+    for row in rows:
+        formatted_videos.append(
+            VideoModel(id=row[0], video_title=row[1], video_url=row[2])
+        )
+
+    cur.close()
+    conn.close()
+    return formatted_videos
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port="8000", reload=False)
+    uvicorn.run(app, host='localhost', port="8000", reload=False)
