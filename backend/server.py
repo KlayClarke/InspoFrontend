275df1192f351905f93ee0dc0cc9b6028d1a52a7
Klay Clarke <clarkeklay@gmail.com>
2022-05-29 17:35:14 -0400

    create post route that uploads video to s3 and saves vid url in postgres db

--- backend/server.py
+++ backend/server.py
@@ -15,6 +15,7 @@ load_dotenv()
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
 
 class VideoModel(BaseModel):
@@ -47,15 +48,38 @@ async def get_videos():
     cur = conn.cursor()
     cur.execute("SELECT * FROM video ORDER BY id DESC")
     rows = cur.fetchall()
-
     formatted_videos = []
     for row in rows:
         formatted_videos.append(
             VideoModel(id=row[0], video_title=row[1], video_url=row[2])
         )
-
     cur.close()
     conn.close()
     return formatted_videos
+
+
+@app.post('/videos', status_code=200)
+async def add_video(file: UploadFile):
+    # upload file to AWS S3
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(S3_BUCKET_NAME)
+    bucket.upload_fileobj(file.file, file.filename,
+                          ExtraArgs={"ACL": "public-read"})
+    uploaded_file_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{file.filename}"
+    # store url in postgres database
+    # connect to database
+    conn = psycopg2.connect(
+        database=POSTGRES_DB, user=POSTGRES_USER, password=POSTGRES_PASSWORD, host="localhost",
+    )
+    cur = conn.cursor()
+    cur.execute(
+        f"INSERT INTO video (video_title, video_url) VALUES ('{file.filename}', '{uploaded_file_url}')"
+
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port="8000", reload=False)
