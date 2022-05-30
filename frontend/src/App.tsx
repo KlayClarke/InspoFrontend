9cd5636058836e1921f2115ae4d9ae5fd3c79572
Klay Clarke <clarkeklay@gmail.com>
2022-05-29 20:22:36 -0400

    handle file input / attempt to handle file upload (user clicks upload and spinner plays until either the upload fails or is successful)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,18 +7,57 @@ import {
   Input,
   Button,
   SimpleGrid,
+  Spinner,
 } from "@chakra-ui/react";
+import { upload } from "@testing-library/user-event/dist/upload";
 import { useEffect, useState } from "react";
 
 function App() {
   const [isSelected, setIsSelected] = useState(false);
-  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [allVideos, setAllVideos] = useState<any>([]);
   const [uploadSuccess, setUploadSuccess] = useState(false);
   const [showSpinner, setShowSpinner] = useState(false);
 
+  function onInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    e.target.files?.length && setSelectedFile(e.target.files[0]);
+    setIsSelected(true);
+  }
+
+  function onFileUpload(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    setShowSpinner(true);
+    const formData = new FormData();
+    formData.append("file", selectedFile!, selectedFile?.name);
+    try {
+      fetch(process.env.REACT_APP_API_ENDPOINT + "videos", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("success!");
+          setUploadSuccess(!uploadSuccess);
+          setShowSpinner(false);
+        })
+        .catch((error) => {
+          setShowSpinner(false);
+          console.log(error);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   useEffect(() => {
-    fetch("https://localhost:8000/videos")
+    fetch(process.env.REACT_APP_API_ENDPOINT + "status")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_ENDPOINT + "videos")
       .then((response) => response.json())
       .then((data) => {
         setAllVideos(data);
@@ -35,12 +74,21 @@ function App() {
             <input
               title="video upload"
               type="file"
-              onChange={() => {}}
-              onClick={() => {}}
+              onChange={onInputChange}
             ></input>
-            <Button size={"lg"} colorScheme="blue" disabled>
+            <Button
+              size={"lg"}
+              colorScheme="blue"
+              isDisabled={!isSelected}
+              onClick={onFileUpload}
+            >
               Upload Video
             </Button>
+            {showSpinner && (
+              <Center>
+                <Spinner size={"md"}></Spinner>
+              </Center>
+            )}
           </HStack>
           <Heading>Sponsored Videos</Heading>
           <SimpleGrid columns={3} spacing={8}>
@@ -53,6 +101,7 @@ function App() {
                     controls
                     loop
                     preload="auto"
+                    muted
                   ></video>
                 );
               })}
